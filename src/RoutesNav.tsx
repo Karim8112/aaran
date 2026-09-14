3b34ba7af37a60bedaff6491f43d6c0c7e2c9928
Karim8112 <kareem.happal@gmail.com>
2026-09-14 23:21:53 +0300

    add

--- src/RoutesNav.tsx
+++ src/RoutesNav.tsx
@@ -5,22 +5,25 @@ import Home from "./pages/Home/Home.tsx";
 import Projects from "./pages/Projects/Projects.tsx";
 import Team from "./pages/Team/Team.tsx";
 import MemberDetailPage from "./pages/Team/Profile.tsx";
+import TransitionProvider from "./providers/TransitionProvider.tsx";
 import { useParams } from "react-router-dom";
 
 function RoutesNav() {
   const { memberId } = useParams<{ memberId: string }>();
   return (
     <>
-      <div className="container relative">
-        <Navbar />
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/team" element={<Team />} />
-        <Route path={`/team/:${memberId}`} element={<MemberDetailPage />} />
-      </Routes>
+      <TransitionProvider>
+        <div className="container relative">
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/team" element={<Team />} />
+          <Route path={`/team/:${memberId}`} element={<MemberDetailPage />} />
+        </Routes>
+      </TransitionProvider>
     </>
   );
 }
